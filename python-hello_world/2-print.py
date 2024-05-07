6767f90d196b72bc84a53c32aacf1f8fbab56a5a
Kryss <kryssbm99@gmail.com>
2024-05-07 10:42:16 -0400

    fix

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def main():
-    print("\"Programming is like building a multilingual puzzle\n")
+    print("\"Programming is like building a multilingual puzzle")
+    print("\n")
 
 
 if __name__ == "__main__":
