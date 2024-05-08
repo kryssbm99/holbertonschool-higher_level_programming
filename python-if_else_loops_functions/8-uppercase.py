cb00a02fcf032fa19afcb9e1c48d28328be4a108
Kryss <kryssbm99@gmail.com>
2024-05-08 18:04:31 -0400

    8# Testing

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(s):
+    result = ""
+    for char in s:
+        if 'a' <= char <= 'z':
+            uppercase_char = chr(ord(char) - 32)
+            result += uppercase_char
+        else:
+            result += char
+    print(result)
