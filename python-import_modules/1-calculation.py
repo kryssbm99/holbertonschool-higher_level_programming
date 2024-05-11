c947cc15ada49e6e7a05483c6196ca839af17c52
Kryss <kryssbm99@gmail.com>
2024-05-10 23:08:54 -0400

    Testing

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -9,13 +9,13 @@ b = 5
 if __name__ == "__main__":
 
     result = add(a, b)
-    print("{} + {} = {}".format(a, b, results))
+    print("{} + {} = {}".format(a, b, result))
 
     result = sub(a, b)
-    print("{} - {} = {}".format(a, b, results))
+    print("{} - {} = {}".format(a, b, result))
 
     result = div(a, b)
-    print("{} / {} = {}".format(a, b, results))
+    print("{} / {} = {}".format(a, b, result))
 
     result = mul(a, b)
-    print("{} * {} = {}".format(a, b, results))
+    print("{} * {} = {}".format(a, b, result))
