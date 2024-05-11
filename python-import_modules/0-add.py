ed09c5f148e219c776086fd727dddb7327d2e343
Kryss <kryssbm99@gmail.com>
2024-05-10 20:47:31 -0400

    fix

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-from add_0 import add
+def main():
+    from add_0 import add
 
-a = 1
-b = 2
+    a = 1
+    b = 2
 
-result = add(a, b)
+    result = add(a, b)
 
 
-print(f"{a} + {b} = {result}")
+    print(f"{a} + {b} = {result}")
+
+if __name__ == "__main__":
+        main()
