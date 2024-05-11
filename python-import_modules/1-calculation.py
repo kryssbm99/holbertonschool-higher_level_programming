bad82e6fd1933766c097aae23e494dd0c9940a60
Kryss <kryssbm99@gmail.com>
2024-05-10 23:18:04 -0400

    testing

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, div, mul
+from calculator_1 import add, sub, mul, div
 
 
 a = 10
@@ -10,12 +10,9 @@ if __name__ == "__main__":
 
     result = add(a, b)
     print("{} + {} = {}".format(a, b, result))
-
     result = sub(a, b)
     print("{} - {} = {}".format(a, b, result))
-
-    result = div(a, b)
-    print("{} / {} = {}".format(a, b, result))
-
     result = mul(a, b)
     print("{} * {} = {}".format(a, b, result))
+    result = div(a, b)
+    print("{} / {} = {}".format(a, b, result))
