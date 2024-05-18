01b28ab6857687f53a9eaf3d6f8bcfbb58fdac96
Kryss <kryssbm99@gmail.com>
2024-05-17 20:23:58 -0400

    fix

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
 """
-This module defines an empty Square class.
+
+This module defines an empty Square class
+
 """
+
 
 class Square:
     """
-    An empty class that defines a square.
+
+    An empty class that defines a square
+    
+    This class is empty
+
     """
     pass
