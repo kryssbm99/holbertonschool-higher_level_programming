05d8729225c0633c076a94c854eb408da4cce856
Kryss <kryssbm99@gmail.com>
2024-05-24 12:18:22 -0400

    1 fix negative radius

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -24,7 +24,7 @@ Implement the Circle class
 
 class Circle(Shape):
     def __init__(self, radius):
-        self.radius = radius
+        self.radius = abs(radius)
 
     def area(self):
         return math.pi * self.radius ** 2
