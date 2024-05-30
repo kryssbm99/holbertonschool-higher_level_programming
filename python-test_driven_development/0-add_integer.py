aab6ffc57f701418c921f59d0eac626b0c029ba1
Kryss <kryssbm99@gmail.com>
2024-05-30 11:01:31 -0400

    fix

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+"""
+Function adds two integers
+"""
+
 def add_integer(a, b=98):
     """
-    Adds two integers.
     This function takes two arguments, 'a' and 'b',
     which must be either integers or floats.
     If the arguments are floats,
