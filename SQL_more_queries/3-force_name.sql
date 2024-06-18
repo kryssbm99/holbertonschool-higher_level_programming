b624947794797ab997da721ac534adce33d8b98d
Kryss <kryssbm99@gmail.com>
2024-06-18 09:31:46 -0400

    fixing

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,6 +1,5 @@
 -- Create table force_name (if it doesn't already exist)
-CREATE TABLE IF NOT EXISTS `{{ database_name }}`.`force_name` (
-  `id` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS force_name (
+  `id` INT,
   `name` VARCHAR(256) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+);
--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,9 +1,9 @@
 -- List California cities (no JOIN, using subquery)
-SELECT c.id, c.name
-FROM `{{ database_name }}`.`cities` AS c
-WHERE c.state_id = (
+SELECT cities.id, cities.name
+FROM `{{ database_name }}`.`cities`
+WHERE cities.state_id = (
   SELECT id
   FROM `{{ database_name }}`.`states`
   WHERE name = 'California'
 );
-ORDER BY c.id ASC;
+ORDER BY cities.id ASC;
