51de18f5f851c730b396e0c966e4e9ed11702cdf
Kryss <kryssbm99@gmail.com>
2024-06-18 09:34:53 -0400

    Fix #4

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,6 +1,5 @@
 -- Create table unique_id (if it doesn't already exist)
-CREATE TABLE IF NOT EXISTS `{{ database_name }}`.`unique_id` (
-  `id` INT NOT NULL AUTO_INCREMENT,  -- Use AUTO_INCREMENT for unique IDs
-  `name` VARCHAR(256),
-  PRIMARY KEY (`id`)
+CREATE TABLE IF NOT EXISTS unique_id (
+  `id` INT,
+  `name` VARCHAR(256)
 );
