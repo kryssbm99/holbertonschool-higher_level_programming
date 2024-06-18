68dca5babd856034e42ff6975108c851c6c40f44
Kryss <kryssbm99@gmail.com>
2024-06-18 08:58:20 -0400

    Task #1 fix

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,5 @@
--- Create user user_0d_1 with secure password (replace 'password' with your desired password)
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'password123';
+-- Create user user_0d_1 with secure password (replace with your desired password)
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
+-- Grant all privileges on *.* to user_0d_1
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
