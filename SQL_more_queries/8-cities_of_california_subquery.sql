2ccf1f5078a1bf0e5ff48fa3042284ad7975f7af
Kryss <kryssbm99@gmail.com>
2024-06-18 09:18:13 -0400

    Fixing stuff

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,9 +1,9 @@
--- Get California state ID (assuming it's unique)
-SELECT id FROM `hbtn_0d_usa`.`states` WHERE name = 'California';
-
--- Set variable to store California state ID (if found)
-SET california_state_id = (SELECT id FROM `hbtn_0d_usa`.`states` WHERE name = 'California');
-
--- List cities in California (if California state ID exists)
-SELECT * FROM `hbtn_0d_usa`.`cities` WHERE state_id = california_state_id
-ORDER BY id ASC;
+-- List California cities (no JOIN, using subquery)
+SELECT c.id, c.name
+FROM `{{ database_name }}`.`cities` AS c
+WHERE c.state_id = (
+  SELECT id
+  FROM `{{ database_name }}`.`states`
+  WHERE name = 'California'
+);
+ORDER BY c.id ASC;
