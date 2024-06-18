d7c8f20f9cdbba6e9050544c5c8ded1fec1cc8be
Kryss <kryssbm99@gmail.com>
2024-06-18 16:24:11 -0400

    Applying final fixes

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- List all cities with state names (using a single SELECT)
-SELECT c.id, c.name, s.name AS state_name
-FROM `{{ database_name }}`.`cities` AS c
-INNER JOIN `{{ database_name }}`.`states` AS s ON c.state_id = s.id
-ORDER BY c.id ASC;
+SELECT cities.id, cities.name, states.name
+FROM cities
+JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
