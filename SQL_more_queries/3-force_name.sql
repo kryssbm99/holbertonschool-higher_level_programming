42f58053ec398e8bf2f50cd4c98eab0497301774
Kryss <kryssbm99@gmail.com>
2024-06-18 09:01:40 -0400

    Task #3 fix

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,6 +1,6 @@
 -- Create table force_name (if it doesn't already exist)
 CREATE TABLE IF NOT EXISTS `{{ database_name }}`.`force_name` (
-  `id` INT,
-  `name` VARCHAR(256),
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(256) NOT NULL,
   PRIMARY KEY (`id`)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
