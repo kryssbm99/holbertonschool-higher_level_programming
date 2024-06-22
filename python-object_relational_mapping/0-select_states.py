ede93f033f39186bc55aa6b893a42828acc43302
Kryss <kryssbm99@gmail.com>
2024-06-21 20:14:30 -0400

    Task #0 pycode fix

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,6 +5,7 @@ Module to list all states from the database hbtn_0e_0_usa
 import MySQLdb
 import sys
 
+
 def list_states(username, password, dbname):
     """
     Connects to the database and prints all states sorted by id.
@@ -33,11 +34,10 @@ def list_states(username, password, dbname):
     cursor.close()
     db.close()
 
+
 if __name__ == "__main__":
     if len(sys.argv) == 4:
         username = sys.argv[1]
         password = sys.argv[2]
         dbname = sys.argv[3]
         list_states(username, password, dbname)
-    else:
-        print("Usage: ./0-select_states.py <mysql username> <mysql password> <database name>")
