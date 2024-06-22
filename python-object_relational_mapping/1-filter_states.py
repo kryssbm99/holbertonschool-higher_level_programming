bedcc5a536f5d5f53e2e99190cf654890f62f028
Kryss <kryssbm99@gmail.com>
2024-06-21 20:21:39 -0400

    Task #1

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+Module to list all states with a name
+starting with 'N' from the database hbtn_0e_0_usa
+"""
+import MySQLdb
+import sys
+
+
+def filter_states(username, password, dbname):
+    """
+    Connects to the database and prints all
+    states with a name starting with 'N' sorted by id.
+    """
+    # Connect to the MySQL database
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=dbname
+    )
+    cursor = db.cursor()
+
+    # Execute the SQL query
+    cursor.execute("id, name FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+
+    # Fetch all the results
+    states = cursor.fetchall()
+
+    # Print each state
+    for state in states:
+        print(f"({state[0]}, '{state[1]}')")
+
+    # Close the cursor and connection
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 4:
+        username = sys.argv[1]
+        password = sys.argv[2]
+        dbname = sys.argv[3]
+        filter_states(username, password, dbname)
