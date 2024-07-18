f9d54d5b4f5eb2748eb2d1689dd81a83f5af91ad
Kryss <kryssbm99@gmail.com>
2024-07-18 18:10:28 -0400

    Task #3-4

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -31,11 +31,33 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        """
-        Set the height of the rectangle
-        """
+        """Set the height of the rectangle"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+    def area(self):
+        """Calculate and return the area of the rectangle"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Calculate and return the perimeter of the rectangle
+        If width or height is equal to 0, the perimeter is 0
+        """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """Return a string representation of the rectangle
+        If width or height is equal to 0, return an empty string
+        """
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        return "\n".join(["#" * self.__width for _ in range(self.__height)])
+
+    def __repr__(self):
+        """Return a string representation of the rectangle to recreate a new instance"""
+        return f"Rectangle({self.__width}, {self.__height})"
