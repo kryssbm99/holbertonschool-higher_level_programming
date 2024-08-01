53997bd57d138e5c1b592568c92d6746c84ec04e
Kryss <kryssbm99@gmail.com>
2024-08-01 16:36:09 -0400

    #4 final fix

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -8,6 +8,6 @@ import json
 
 
 def from_json_string(my_str):
-    """Returns an object (Python data structure) represented by a 
+    """Returns an object (Python data structure) represented by a
     JSON string."""
     return json.loads(my_str)
